127b6f4d72009f31636f8319653cdfe5a5f82096
Jadiael <Jadiael@hotmail.com.br>
2025-05-10 16:23:50 -0300

    Add updateUserActiveStatus function and params to getUsers function

--- src/api/endpoints/users.ts
+++ src/api/endpoints/users.ts
@@ -1,11 +1,22 @@
 import api from "../axios";
 import { IApiResponseBase, IApiResponseBasePaginate, IUser } from "../../types";
 
-export const getUsers = async (): Promise<
-  IApiResponseBase<IApiResponseBasePaginate<IUser[]>>
-> => {
+export const getUsers = async (
+  params = {}
+): Promise<IApiResponseBase<IApiResponseBasePaginate<IUser[]>>> => {
   const response = await api.get<
     IApiResponseBase<IApiResponseBasePaginate<IUser[]>>
-  >("/users");
+  >("/users", { params });
+  return response.data;
+};
+
+export const updateUserActiveStatus = async (
+  id: number,
+  active: boolean
+): Promise<IApiResponseBase<IUser>> => {
+  const response = await api.patch<IApiResponseBase<IUser>>(
+    `/users/${id}/active`,
+    { active }
+  );
   return response.data;
 };
