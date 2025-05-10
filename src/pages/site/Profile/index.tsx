42c050286e69bbd68c9f820be6447720e79e2260
Jadiael <Jadiael@hotmail.com.br>
2025-05-10 17:58:50 -0300

    Add new avatar placeholders and update Profile image import path

--- src/pages/site/Profile/index.tsx
+++ src/pages/site/Profile/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../hooks/useAuth";
 import styles from "./assets/css/Profile.module.css";
-import avatarPlaceHolder from "../../../assets/images/avatar_placeholder.png";
+import avatarPlaceHolder from "../../../assets/images/avatar_placeholder_2.png";
 import {
   getTransactions,
   TTransactionData,
